00ce0f3f7def85c8b07634295f670614fb390070
woshiguaiguai <1565228530@qq.com>
2017-03-11 20:09:16 +0800

    new class

--- app/src/main/java/com/googl/xcdq/videonews/MainActivity.java
+++ app/src/main/java/com/googl/xcdq/videonews/MainActivity.java
@@ -3,6 +3,8 @@ package com.googl.xcdq.videonews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.vov.vitamio.MediaPlayer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
