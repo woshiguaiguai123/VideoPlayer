e8866fc6dc535a5c5298f2a9c4e5829b5326b7e7
woshiguaiguai <1565228530@qq.com>
2017-03-11 20:03:29 +0800

    kkkkk

---
+++ RecycleViewTest/src/main/java/com/googl/xcdq/videonwes/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.googl.xcdq.videonwes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        initRecycleView();
+
+    }
+
+    private void initRecycleView() {
+
+    }
+}
---
+++ app/src/main/java/com/googl/xcdq/videonews/ui/news/comments.java
@@ -0,0 +1,8 @@
+package com.googl.xcdq.videonews.ui.news;
+
+/**
+ * Created by xcdq on 2017/3/10.
+ */
+
+public class comments {
+}
